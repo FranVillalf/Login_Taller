33fe92eb323e99d306b4ba4c61cc6c52560dcd3f
fvillegasa <fvillegasa@PREP000WTE114>
2024-11-14 13:07:42 -0600

    Se data por json

---
+++ Login_Taller/Genericos/LeerJson.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Esta clase deserealiza un json
+namespace Login_Taller.Genericos 
+{
+    public class LeerJson
+    {
+        public POJO.LoginData Login_data()
+        {
+            var json = JsonConvert.DeserializeObject<POJO.LoginData>(File.ReadAllText(@"..\ ..\ Data\login.json"));
+            return json;
+        }
+    }
+}
